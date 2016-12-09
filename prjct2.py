1cfddda44a4e1da5b5ea6cfcb4d67ec29682d04f
root <root@ip-172-31-45-189.us-west-2.compute.internal>
2016-12-09 01:14:37 +0000

    updated

--- prjct2.py
+++ prjct2.py
@@ -15,7 +15,7 @@ mysql = MySQL()
 
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_PASSWORD'] = 'toor'
 app.config['MYSQL_DATABASE_DB'] = 'test'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
